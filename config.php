fefc3e7a1ba262421ed9af03a77f6ea8506d83a0
tegat48 <45688062+tegat48@users.noreply.github.com>
2018-12-07 18:44:21 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+$ticket = 
+"sHp2r312otmCpnGns3mCqJhovNiBh4-Wr7qGpLTbmJSxeqCwfp-6ko62mJvAnnmshH28z396hM-6mIWxv7Wcl62jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ";
+$meid = "12020c19887bcbbc";
+$os="android";
+$sign="6c5b3a5cb6b854745f073ccfbbc00e18635861a4af87c1e9af907660bd7b2a56";
